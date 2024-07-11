cfa655dc1a5739d901eed35f6b4e6151eb6ba56d
sumit0412 <sumit.sachdev412@gmail.com>
2024-07-11 18:51:42 +0700

    Testing execution time of normal vs parallel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+mod parallel;
+
+use std::time::Instant;
+use parallel::{normal_scan, parallel_scan};
+
 fn main() {
-    println!("Hello, world!");
+    let data: Vec<i32> = (0..1_000_000).collect();
+
+    // Measuring the normal code execution time
+    let start = Instant::now();
+    let result_normal = normal_scan(&data, |x| x + 1);
+    let duration_normal = start.elapsed();
+    println!("Normal execution time: {:?}", duration_normal);
+
+    // Measuring the parallel code execution time
+    let start = Instant::now();
+    let result_parallel = parallel_scan(&data, |x| x + 1);
+    let duration_parallel = start.elapsed();
+    println!("Parallel execution time: {:?}", duration_parallel);
+
+    // Making sure that both methods produce the same result because parallel code might have data race or some other error which can affect the result
+    assert_eq!(result_normal, result_parallel);
 }
--- src/parallel.rs
+++ src/parallel.rs
@@ -1,5 +1,13 @@
 use rayon::prelude::*;
 
+pub fn normal_scan<T, F>(data: &[T], process: F) -> Vec<T>
+where
+    T: Clone,
+    F: Fn(&T) -> T,
+{
+    data.iter().map(process).collect()
+}
+
 pub fn parallel_scan<T, F>(data: &[T], process: F) -> Vec<T>
 where
     T: Send + Sync + Clone,
